0f71168306b3ea86a0fb288647f78bec43ae94d1
kewzen <102579525+kewzen@users.noreply.github.com>
2022-09-13 19:26:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-
+let resulttimes = 0;
 let number = 1000
 let counter = 0
 let captcharigh = false;    
+let statustimer = false;
+let s = 0
+let mi = 0
+var capt = document.getElementById("captcha")
 
+
+const seconds = () =>{
+    
+}
+
+
+const timer = () =>{
+ mi = mi +  10 
+ if (mi == 1000 || mi > 1000){
+    s++
+    mi = mi - 1000
+ }
+
+  resulttimes = s +"."+mi
+
+
+}
 
 
 
@@ -13,19 +34,34 @@ function getRandomInt(min, max) {
   }
 
   let conv = 0
+  
 
 
 
 
- const captcha = () =>{
-   var elem = document.getElementById("result")
+
+var inttimer;
+
+
+  const captcha = () =>{
+  
+
+   
+        
     var vis = document.getElementById("captcha")
+
+ 
+   
+   var elem = document.getElementById("result")
+    
     vis.style.display = "block"
     elem.style.display = "block"
     var inp = document.getElementById("inp")
    inp.focus();
   
-    
+    if (vis.style.display == "block"){
+        inttimer = setInterval(timer,10)
+    }
     
 
     while (counter < 4){
@@ -43,48 +79,64 @@ function getRandomInt(min, max) {
 
        var elem = document.getElementById("captchanum")
        elem.textContent = conv 
-time = 0.000
+
 }
+window.onload = () =>{
+    while (vis.style.display == "none"){
+        clearInterval(inttimer)
+        console.log("puk")
+    }
+}
+
+
 
 const ch = e =>{
    
-    if (e.value == 'n')
+    if (e.value == 'n' || e.value == 'т')
     {
         e.value = ''
     } 
+
+  
 }
 const submitcaptcha = ()  =>{
     var elem = document.getElementById("result")
     var vis = document.getElementById("captcha")
     var answ = document.getElementById("inp")
     var elem2 = document.getElementById("result")
-    if (answ.value == conv){
-        captcharight = true
-        
-       
-       
-
-    }
-    elem2.textContent = answ.value + "|" +conv
+     clearInterval(inttimer)
+     vis.style.display = "none"
+     elem.style.display = "block"
 
    
-    answ.value = ""
-    time = 0.000
-    conv = ""
-    vis.style.display = "none"
-    elem.style.display = "block"
+    console.log(mi)
+    elem2.textContent = answ.value + "|" +conv
+    document.getElementById("resulttime").textContent = resulttimes
+     answ.value = ""
+     if (s > 0 || mi > 0 || resulttimes > 0){
+     s = 0
+     mi = 0
+     resulttimes = 0
+     }
+     conv = ""
+
+   
+  
    
 }
+
+
+
  
 
 document.addEventListener('keydown', function(event){
     if (event.keyCode == 78){
         captcha()
+        
     }
     if (event.keyCode == 13){
         submitcaptcha()
-        
-        
+       
     }
     // console.log('Key: ', event.key);
     // console.log('keyCode: ', event.keyCode);
